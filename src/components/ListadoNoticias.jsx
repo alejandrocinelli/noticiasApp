54a181feaad5aab0a4b2af656d2dcefeba215d71
alejandroCinelli <alejandro.cinelli@hotmail.com>
2022-08-19 23:17:08 -0300

    3erCommit

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,28 +1,43 @@
-import React from "react"
-import useNoticias from '../hooks/useNoticias'
-import { Grid,Typography } from "@mui/material"
-import Noticias from "./Noticias"
+import React from "react";
+import useNoticias from "../hooks/useNoticias";
+import { Grid, Typography } from "@mui/material";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import Noticias from "./Noticias";
 
 const ListadoNoticias = () => {
-    const {noticias} = useNoticias()
-    console.log(noticias)
+  const { noticias,totalPaginas,handlerChangePagintion,pagina } = useNoticias();
+  //console.log(noticias)
 
   return (
     <>
-    <Typography
-    textAlign="center" 
-    marginY={5}
-    component="h2" variant="h3"
-      >
+      <Typography textAlign="center" marginY={5} component="h2" variant="h3">
         Ultimas Noticias
-    </Typography>
+      </Typography>
 
-    <Grid container spacing={2} >
-        {noticias.map(noticia => (
-            <Noticias key={noticia.url} noticia={noticia}/>))}
-    </Grid>
+      <Grid container spacing={2}>
+        {noticias.map((noticia) => (
+          <Noticias key={noticia.url} noticia={noticia} />
+        ))}
+      </Grid>
 
+      <Stack spacing={2}
+      sx={{
+          marginTop: '2rem',
+          marginBottom: '3rem',
+      }}
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      >
+        <Pagination 
+        count={totalPaginas} color="primary"
+        onChange={handlerChangePagintion}
+        page={pagina}
+        
+        />
+      </Stack>
     </>
-  )
-}
-export default ListadoNoticias
+  );
+};
+export default ListadoNoticias;
--- src/contex/NoticiasProvider.jsx
+++ src/contex/NoticiasProvider.jsx
@@ -5,8 +5,9 @@ const NoticiasContext = createContext();
 const NoticiasProvider = ({ children }) => {
 
  const [categoria,setCategoria] = useState("general");
-
  const [noticias, setNoticias] = useState([])
+ const [pagina, setPagina] = useState(1)
+ const [totalPaginas, setTotalPaginas] = useState(0)
 
  useEffect(() => {
 
@@ -15,16 +16,40 @@ const NoticiasProvider = ({ children }) => {
          const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
          const respuesta = await fetch(url)
          const data = await respuesta.json()
-       //  console.log(data)
+        //  console.log(data)
          setNoticias(data.articles)
+         setTotalPaginas(Math.ceil(data.totalResults/20) )
+         setPagina(1)
      }
 
      consultarApi()
 
  } , [categoria])
 
+ useEffect(() => {
+
+    const consultarApi = async () => {
+
+        const url = `https://newsapi.org/v2/top-headlines?country=ar&page=${pagina}&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+        const respuesta = await fetch(url)
+        const data = await respuesta.json()
+       //  console.log(data)
+        setNoticias(data.articles)
+        setTotalPaginas(Math.ceil(data.totalResults/20) )
+    }
+
+    consultarApi()
+
+} , [pagina]) 
+
  const handlerCategoria = (e) => {
         setCategoria(e.target.value);
+ }
+
+ const handlerChangePagintion = (e,value) => {
+    //console.log(e.target.textContent) uso el textContent para obtener el numero de la pagina por que no tiene value
+    //console.log(value) puedo usar value por que llega un evento y un valor
+    setPagina(value)
  }
 
     return (
@@ -33,7 +58,10 @@ const NoticiasProvider = ({ children }) => {
     value={{
         categoria,
         handlerCategoria,
-        noticias
+        noticias,
+        totalPaginas,
+        handlerChangePagintion,
+        pagina
 
     }}
     >
